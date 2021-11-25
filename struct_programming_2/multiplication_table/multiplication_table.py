b72e03f61547c6998cad07a32fc414a1ebe3c98f
Cole.sl <90853900+Astroblist@users.noreply.github.com>
2021-11-25 09:43:45 -0700

    Add files via upload

--- struct_programming_2/multiplication_table/multiplication_table.py
+++ struct_programming_2/multiplication_table/multiplication_table.py
@@ -1,31 +1,19 @@
-#simple program to make a multiplication table 
+start_factor = int(input("Starting number ?: "))
+end_factor = int(input("Ending number ?: "))
+start_number = int(input("Starting Factor ?: "))
+end_number = int(input("Ending Factor ?: "))
 
-#gets parameters from user then converts into list
-def get_params():
-    start_number = int(input("Starting number: "))
-    end_number = int(input("Ending number: "))
+output = '    '
 
-    start_factor = int(input("Starting factor: "))
-    end_factor = int(input("Ending factor: "))
+if start_number <= end_number:
+    for num in range(start_number, end_number + 1):
+        output += '{:>5}'.format(str(num))
+    print('{:8}'.format(output))
+    print("---" + "-" * 5 * (end_number - start_number + 1))
 
-    #returns 2 lists (number, factor) of ranges based on the inputs
-    return list(range(start_number, end_number + 1)), list(range(start_factor, end_factor + 1))
-
-def create_data(numbers, factors):
-    multiple_dict = {'numbers': numbers, 'factors':factors}
-
-    for x in factors:
-        factor_list = []
-        for y in numbers:
-            factor_list.append(x*y)
-
-        multiple_dict[x] = factor_list
-
-    return multiple_dict
-
-def format_data():
-    pass
-
-
-numbers, factors = get_params()
-print(create_data(numbers, factors))
+for row in range(start_factor, end_factor + 1):
+    output = ""
+    output += '{0:>3}'.format(str(row) + ":")
+    for col in range(start_number, end_number + 1):
+        output += '{0:>5}'.format(str(col*row))
+    print(output)
